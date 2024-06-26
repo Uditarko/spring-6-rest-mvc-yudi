b62b8abdb1b799eee390fc7613ad7d57b74bcfe2
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-06-26 15:15:39 +0530

    adding mockito matchers BeerControllerTest.getBeerById

--- src/test/java/guru/springframework/yudi/spring6restmvcyudi/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/yudi/spring6restmvcyudi/controllers/BeerControllerTest.java
@@ -12,11 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(BeerController.class)
@@ -39,6 +39,8 @@ class BeerControllerTest {
         mockMvc.perform(get("/api/v1/beer/"+UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 }
