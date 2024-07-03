27036580d3d066cb61dbf42439ab7664a4689508
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-07-03 14:08:27 +0530

    created error message for validation failure

--- src/main/java/guru/springframework/yudi/spring6restmvcyudi/controllers/CustomErrorController.java
+++ src/main/java/guru/springframework/yudi/spring6restmvcyudi/controllers/CustomErrorController.java
@@ -5,10 +5,21 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ControllerAdvice
 public class CustomErrorController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException methodArgumentNotValidException){
-        return ResponseEntity.badRequest().body(methodArgumentNotValidException.getBindingResult().getFieldErrors());
+        List<Map<String,String>> errorList = methodArgumentNotValidException.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap =  new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).toList();
+
+        return ResponseEntity.badRequest().body(errorList);
     }
 }
