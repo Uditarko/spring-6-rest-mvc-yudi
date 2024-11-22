7f2014ea3c7bf9f3fa5936163723f0b7ea6ba8b7
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-11-22 12:43:45 +0530

    adding email to customer

--- src/main/java/guru/springframework/yudi/spring6restmvcyudi/entities/Customer.java
+++ src/main/java/guru/springframework/yudi/spring6restmvcyudi/entities/Customer.java
@@ -23,6 +23,10 @@ public class Customer {
     @JdbcTypeCode(SqlTypes.CHAR)
     private UUID id;
     private String name;
+
+    @Column(length = 255)
+    private String email;
+
     @Version
     private Integer version;
     private LocalDateTime createdDate;
