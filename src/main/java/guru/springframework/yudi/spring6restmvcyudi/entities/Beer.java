9f636e84bda92f742111c218245cf9702f2ed10b
Uditarko Dutta <uditarko.dutta@gmail.com>
2024-11-22 13:13:19 +0530

    adding csv file for beer data

--- src/main/java/guru/springframework/yudi/spring6restmvcyudi/entities/Beer.java
+++ src/main/java/guru/springframework/yudi/spring6restmvcyudi/entities/Beer.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
